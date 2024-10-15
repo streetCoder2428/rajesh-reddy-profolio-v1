8b94a7755cb805e4b7d023ed199fbe5e7b9ed93a
Venkata Rajesh Kumar Reddy Kuncham <reddy.rajesh@qnulabs.com>
2024-10-15 18:13:25 +0530

    new commit 15/Oct

--- src/components/myprojects/Myprojects.jsx
+++ src/components/myprojects/Myprojects.jsx
@@ -1,8 +1,5 @@
 import React from "react";
 import "./myprojects.css";
-import IMG1 from "../../assets/img5.jpg";
-import IMG2 from "../../assets/img2.png";
-import IMG3 from "../../assets/img3.png";
 import Profolio from "../../assets/profolio.png";
 
 const Myprojects = () => {
@@ -12,38 +9,6 @@ const Myprojects = () => {
       <h2>Projects</h2>
 
       <div className="container portfolio__container">
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>Fashion Gallery</h3>
-          <small className="text-light">Spring Boot | ReactJs | MySQL</small>
-          <div className="portfolio__item-cta">
-            <a
-              href="https://github.com/himani413/FashionGallery"
-              target="_blank"
-              rel="noreferrer"
-              className="btn">
-              Github
-            </a>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG2} alt="" />
-          </div>
-          <h3>Beauty Garden</h3>
-          <small className="text-light">HTML | CSS | JS | php | MySQL</small>
-          <div className="portfolio__item-cta">
-            <a
-              href="https://github.com/Thakshila-Bandara/Beauty-Garden"
-              target="_blank"
-              rel="noreferrer"
-              className="btn">
-              Github
-            </a>
-          </div>
-        </article>
         <article className="portfolio__item">
           <div className="portfolio__item-image">
             <img src={Profolio} alt="" />
@@ -59,30 +24,7 @@ const Myprojects = () => {
               Github
             </a>
             <a
-              href="https://rajesh-reddy-kuncham.github.io/my-portfolio"
-              target="_blank"
-              rel="noreferrer"
-              className="btn btn-primary">
-              Visit Website
-            </a>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG3} alt="" />
-          </div>
-          <h3>My Resume</h3>
-          <small className="text-light">HTML | CSS | JS | Bootstrap</small>
-          <div className="portfolio__item-cta">
-            <a
-              href="https://github.com/Thakshila-Bandara/my-resume"
-              target="_blank"
-              rel="noreferrer"
-              className="btn">
-              Github
-            </a>
-            <a
-              href="https://thakshila-bandara.github.io/my-resume/"
+              href="https://streetcoder2428.github.io/rajesh-reddy-profolio-v1/"
               target="_blank"
               rel="noreferrer"
               className="btn btn-primary">
